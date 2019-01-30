442dcee56a1493ae743043c4d4e0d04ec7032afd
Armen Arzumanyan <armena@YLT160015.CORP.OPTYM.NET>
2019-01-31 00:30:24 +0400

    up

--- jdkdemo/src/main/java/io/project/app/jdkdemo/JdkdemoApplication.java
+++ jdkdemo/src/main/java/io/project/app/jdkdemo/JdkdemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JdkdemoApplication {
 
+    // ab -n 100000 -c 1000 http://localhost:9595/api/v2/data
 	public static void main(String[] args) {
 		SpringApplication.run(JdkdemoApplication.class, args);
 	}
--- jdkdemo/src/main/java/io/project/app/jdkdemo/MainController.java
+++ jdkdemo/src/main/java/io/project/app/jdkdemo/MainController.java
@@ -1,5 +1,6 @@
 package io.project.app.jdkdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,18 @@ import reactor.core.publisher.Flux;
  */
 @RestController
 @RequestMapping("/api/v2/data")
+@Slf4j
 public class MainController {
 
     @GetMapping
     public Flux<String> get() {
+
+        log.info("TEST LOG");
+        double result3 = Math.floorDiv(-100, 9);
+        System.out.println("result3: " + result3);
+
+        double result4 = -100 / 9;
+        System.out.println("result4: " + result4);
         return Flux.just("List");
     }
 }
